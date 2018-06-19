c5f3a09f7f5be2e4347f40ac7f0f60066ef93994
Paul Esch-Laurent <herppfel@gmail.com>
2018-06-19 02:33:30 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+{{{appJS}}}
+
+// This script is embedded in the footer of every page
+
+// Thanks to CodePen:
+// https://blog.codepen.io/2016/11/17/anchor-links-post-headers/
+(function() {
+
+  function slugify(text) {
+    return text.toString().toLowerCase().trim()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+      .replace(/\-\-+/g, '-'); // Replace multiple - with single -
+  }
+
+  var $headers = $(".entry :header");
+
+  $headers.each(function(index) {
+
+    var $el = $(this);
+
+    var idToLink = slugify($el.text()) + '-' + index;
+
+    var $headerLink = $("<a />", {
+      html: "#",
+      class: "article-header-hash",
+      href: "#" + idToLink,
+      id: idToLink
+    });
+
+    $el.prepend($headerLink);
+
+  });
+
+}());
