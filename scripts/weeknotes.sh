54ab6df18efca8834505e32ac14d8157b367269d
Paul Esch-Laurent <p@uly.io>
2024-09-03 18:40:40 -0500

    feat(weeknotes.sh): scaffold out empty weeknote

--- scripts/weeknotes.sh
+++ scripts/weeknotes.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-echo -n "Weeknotes: "
-date +%Y-W%W
+yyyyww="$(date +%Y-W%W)"
+
+# TODO: consider just putting in drafts so it matches the normal flow?
+mkdir -p "$(dirname "$0")"/../../../posts/weeknotes
+if ! [[ -w "$(dirname "$0")"/../../../posts/weeknotes/"${yyyyww,,}".md ]]; then
+  {
+    echo '---'
+    echo 'date: TODO FIXME'
+    echo "tags: weeknotes"
+    echo "tldr: Weeknotes for $yyyyww."
+    echo "link: /${yyyyww,,}"
+    echo 'draft: true'
+    echo -e '---\n'
+    echo -e "# Weeknotes for $yyyyww\n"
+    echo 'TODO: fix the date, remove draft'
+  } > "$(dirname "$0")"/../../../posts/weeknotes/"${yyyyww,,}".md
+fi
+
+echo "$yyyyww"
