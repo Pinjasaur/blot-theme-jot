3a0a07193b886699af00d040aaadad754e0ea15e
Paul Esch-Laurent <p@uly.io>
2024-09-05 19:27:34 -0500

    feat(weeknote.shs): scaffold in drafts/ instead

--- scripts/weeknotes.sh
+++ scripts/weeknotes.sh
@@ -2,20 +2,18 @@
 
 yyyyww="$(date +%Y-W%W)"
 
-# TODO: consider just putting in drafts so it matches the normal flow?
-mkdir -p "$(dirname "$0")"/../../../posts/weeknotes
-if ! [[ -w "$(dirname "$0")"/../../../posts/weeknotes/"${yyyyww,,}".md ]]; then
+mkdir -p "$(dirname "$0")"/../../../drafts/-
+if ! [[ -w "$(dirname "$0")"/../../../drafts/-/"${yyyyww,,}".md ]]; then
   {
     echo '---'
     echo 'date: TODO FIXME'
     echo "tags: weeknotes"
     echo "tldr: Weeknotes for $yyyyww."
     echo "link: /${yyyyww,,}"
-    echo 'draft: true'
     echo -e '---\n'
     echo -e "# Weeknotes for $yyyyww\n"
-    echo 'TODO: fix the date, remove draft'
-  } > "$(dirname "$0")"/../../../posts/weeknotes/"${yyyyww,,}".md
+    echo 'TODO: fix the date'
+  } > "$(dirname "$0")"/../../../drafts/-/"${yyyyww,,}".md
 fi
 
 echo "$yyyyww"
